5a3ab7d585266ad2f9ff26b6516afdbc5587fb28
bski006 <boch@umich.edu>
2014-11-06 16:58:21 -0500

    business request

---
+++ app/src/main/java/veme/cario/com/foshop/model/BusinessRequest.java
@@ -0,0 +1,28 @@
+package veme.cario.com.foshop.model;
+
+import com.parse.ParseObject;
+
+/* Implementation note:
+    1. no need for UI, so only setters no getters
+ */
+
+public class BusinessRequest extends ParseObject {
+
+    public enum BusRequestType {
+        Dealership,
+        Service,
+        Upgrade;
+    }
+
+    /* Default empty constructor */
+    public BusinessRequest () {}
+
+    public void setDestination(String destination) {
+        put ("destination", destination);
+    }
+
+    public void setRequestType(BusRequestType busRequestType) {
+        put ("requestType", busRequestType);
+    }
+
+}
