dfc7d9fc5ba52a4cd4383f2ca95378018a22911c
bski006 <boch@umich.edu>
2014-12-15 17:50:15 -0500

    zooming control done

--- app/src/main/java/veme/cario/com/CARmera/view/CameraPreview.java
+++ app/src/main/java/veme/cario/com/CARmera/view/CameraPreview.java
@@ -4,33 +4,91 @@ import android.content.Context;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by bski on 11/6/14.
  */
-public class CameraPreview extends SurfaceView
-        implements SurfaceHolder.Callback {
-
-    /* SurfaceView class provides a surface in which a secondary thread
-        can render into the screen.
-            - Be cautious of SurfaceView and SurfaceHolder.Callback methods coming 
-                from thread running SurfaceView's window, they need to be synchronized
-                with the drawing thread.
-            - Ensure that the drawing thread oly touches the underlying Surface while it's 
-                valid. (surfaceCreated -> surfaceDestroyed)
-    */
-
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
     private Context cxt;
     private SurfaceHolder surfaceHolder;
     private Camera camera;
     private static String TAG = "SURFACE_VIEW";
-    /* camera variables */
-    private int zoom_factor = 0;
 
+    /* zooming variables */
+    private boolean has_zoom = false;
+    private int zoom_factor = 0;
+    private int max_zoom_factor = 0;
+    private ScaleGestureDetector scaleGestureDetector;
+    private List<Integer> zoom_ratios = null;
+    private boolean touch_was_multitouch = false;
+
+    private class ZoomListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            CameraPreview.this.scaleZoom(detector.getScaleFactor());
+            return true;
+        }
+    }
+
+    public void scaleZoom(float scale_factor) {
+        if( this.has_zoom ) {
+            float zoom_ratio = this.zoom_ratios.get(zoom_factor)/100.0f;
+            zoom_ratio *= scale_factor;
+            int new_zoom_factor = zoom_factor;
+            if( zoom_ratio <= 1.0f ) {
+                new_zoom_factor = 0;
+            }
+            else if( zoom_ratio >= zoom_ratios.get(max_zoom_factor)/100.0f ) {
+                new_zoom_factor = max_zoom_factor;
+            }
+            else {
+                if( scale_factor > 1.0f ) {
+                    for(int i=zoom_factor;i<zoom_ratios.size();i++) {
+                        if( zoom_ratios.get(i)/100.0f >= zoom_ratio ) {
+                            new_zoom_factor = i;
+                            break;
+                        }
+                    }
+                } else {
+                    for(int i=zoom_factor;i>=0;i--) {
+                        if( zoom_ratios.get(i)/100.0f <= zoom_ratio ) {
+                            new_zoom_factor = i;
+                            break;
+                        }
+                    }
+                }
+            }
+            zoomTo(new_zoom_factor);
+        }
+    }
+
+    public void zoomTo(int new_zoom_factor) {
+        if( new_zoom_factor < 0 )
+            new_zoom_factor = 0;
+        if( new_zoom_factor > max_zoom_factor )
+            new_zoom_factor = max_zoom_factor;
+        if(new_zoom_factor != zoom_factor) {
+            if( this.has_zoom ) {
+                this.setZoom(new_zoom_factor);
+                zoom_factor = new_zoom_factor;
+//                clearFocusAreas();
+            }
+        }
+    }
+
+    void setZoom(int value) {
+        Camera.Parameters parameters = this.camera.getParameters();
+        Log.d(TAG, "zoom " + parameters.getZoom() + " to " + value);
+        parameters.setZoom(value);
+        camera.setParameters(parameters);
+    }
 
 
     public CameraPreview (Context cxt_, Camera camera_) {
@@ -43,7 +101,66 @@ public class CameraPreview extends SurfaceView
         surfaceHolder.addCallback(this);
         /* deprecated, required on android version < 3.0 */
         surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        Camera.Parameters params = camera.getParameters();
+        has_zoom = params.isZoomSupported();
+        if (has_zoom) {
+            zoom_ratios = params.getZoomRatios();
+            max_zoom_factor = params.getMaxZoom();
+        }
+        scaleGestureDetector = new ScaleGestureDetector(getContext(), new ZoomListener());
+
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        scaleGestureDetector.onTouchEvent(event);
+        if( event.getPointerCount() != 1 ) {
+            touch_was_multitouch = true;
+            return true;
+        }
+        if( event.getAction() != MotionEvent.ACTION_UP ) {
+            if( event.getAction() == MotionEvent.ACTION_DOWN && event.getPointerCount() == 1 ) {
+                touch_was_multitouch = false;
+            }
+            return true;
+        }
+        if( touch_was_multitouch ) {
+            return true;
+        }
+//        if( !this.is_video && this.isTakingPhotoOrOnTimer() ) {
+            // if video, okay to refocus when recording
+//            return true;
+//        }
+
+        // note, we always try to force start the preview (in case is_preview_paused has become false)
+        // except if recording video (firstly, the preview should be running; secondly, we don't want to reset the phase!)
+//        if( !this.is_video ) {
+//            startCameraPreview();
+//        }
+//        cancelAutoFocus();
+
+//        if( camera_controller != null && !this.using_face_detection ) {
+//            this.has_focus_area = false;
+//            ArrayList<CameraController.Area> areas = getAreas(event.getX(), event.getY());
+//            if( camera_controller.setFocusAndMeteringArea(areas) ) {
+//                if( MyDebug.LOG )
+//                    Log.d(TAG, "set focus (and metering?) area");
+//                this.has_focus_area = true;
+//                this.focus_screen_x = (int)event.getX();
+//                this.focus_screen_y = (int)event.getY();
+//            }
+//            else {
+//                if( MyDebug.LOG )
+//                    Log.d(TAG, "didn't set focus area in this mode, may have set metering");
+//                // don't set has_focus_area in this mode
+//            }
+//        }
+
+//        tryAutoFocus(false, true);
+        return true;
+    }
+
 
     @Override
     public void surfaceCreated (SurfaceHolder s_holder) {
