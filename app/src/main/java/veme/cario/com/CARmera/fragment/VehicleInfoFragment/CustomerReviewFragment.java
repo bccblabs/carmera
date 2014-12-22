d8eb5e43ed83869a57bab04e35db2248082317d5
bski006 <boch@umich.edu>
2014-12-22 13:50:14 -0500

    customer reviews

--- app/src/main/java/veme/cario/com/CARmera/fragment/VehicleInfoFragment/CustomerReviewFragment.java
+++ app/src/main/java/veme/cario/com/CARmera/fragment/VehicleInfoFragment/CustomerReviewFragment.java
@@ -6,6 +6,7 @@ import android.text.format.Time;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,21 +16,30 @@ import com.octo.android.robospice.SpiceManager;
 import com.octo.android.robospice.persistence.DurationInMillis;
 import com.octo.android.robospice.persistence.exception.SpiceException;
 import com.octo.android.robospice.request.listener.RequestListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import veme.cario.com.CARmera.R;
 import veme.cario.com.CARmera.model.APIModels.VehicleCustomerReview;
 import veme.cario.com.CARmera.model.Json.CustomerReview;
 import veme.cario.com.CARmera.requests.VehicleCustomerReviewRequest;
+import veme.cario.com.CARmera.util.AnimatedExpandableListView;
 import veme.cario.com.CARmera.util.ReviewListAdapter;
 
 public class CustomerReviewFragment extends Fragment {
 
+    private static String JSON_HASH_KEY;
+    private SpiceManager spiceManager = new SpiceManager(JacksonSpringAndroidSpiceService.class);
+
     private TextView review_info;
     private TextView no_customer_review;
-    private ListView customer_reviews;
-    private ReviewListAdapter reviewListAdapter;
-    private static String JSON_HASH_KEY;
 
-    private SpiceManager spiceManager = new SpiceManager(JacksonSpringAndroidSpiceService.class);
+    private AnimatedExpandableListView customer_review_listview;
+    private ReviewListAdapter review_list_adapter;
+    private List<ReviewListAdapter.ReviewItem> customer_reviews_items = new ArrayList<ReviewListAdapter.ReviewItem>();
+
+
 
     private final class CustomerReviewRequestListener implements RequestListener<VehicleCustomerReview> {
         @Override
@@ -41,13 +51,26 @@ public class CustomerReviewFragment extends Fragment {
         @Override
         public void onRequestSuccess (VehicleCustomerReview vehicleCustomerReview) {
             if (CustomerReviewFragment.this.isAdded()) {
-                for (CustomerReview customerReview : vehicleCustomerReview.getReviews()) {
-                    reviewListAdapter.add(customerReview);
-                }
                 String reviewCount = vehicleCustomerReview.getReviewCount();
                 review_info.setText(vehicleCustomerReview.getAverageRating() + "/5 from "
-                                    + reviewCount + " reviewers." );
-                reviewListAdapter.notifyDataSetChanged();
+                        + reviewCount + " reviewers." );
+
+                for (CustomerReview customerReview : vehicleCustomerReview.getReviews()) {
+                    ReviewListAdapter.ReviewChildItem childItem = new ReviewListAdapter.ReviewChildItem();
+                    childItem.text = customerReview.getText();
+                    childItem.favorite_features = customerReview.getFavoriteFeatures();
+                    childItem.suggested_improvements = customerReview.getSuggestedImprovements();
+
+                    ReviewListAdapter.ReviewItem reviewItem = new ReviewListAdapter.ReviewItem();
+                    reviewItem.name = customerReview.getAuthor().getAuthorName();
+                    reviewItem.rating = customerReview.getAverageRating();
+                    reviewItem.title = customerReview.getTitle();
+
+                    reviewItem.items.add(childItem);
+                    customer_reviews_items.add(reviewItem);
+                }
+                review_list_adapter.setData(customer_reviews_items);
+                review_list_adapter.notifyDataSetChanged();
                 CustomerReviewFragment.this.getActivity().setProgressBarIndeterminateVisibility(false);
             }
         }
@@ -100,12 +123,37 @@ public class CustomerReviewFragment extends Fragment {
     private void initUIComponents () {
         review_info = (TextView) getView().findViewById(R.id.review_info_text_view);
 
-        customer_reviews = (ListView) getView().findViewById(R.id.customer_list_view);
-        reviewListAdapter = new ReviewListAdapter(getActivity());
-        customer_reviews.setAdapter(reviewListAdapter);
+        customer_review_listview = (AnimatedExpandableListView) getView().findViewById(R.id.customer_review_listview);
+        review_list_adapter = new ReviewListAdapter(getActivity());
+        review_list_adapter.setData(customer_reviews_items);
+
+        customer_review_listview.setAdapter(review_list_adapter);
 
         no_customer_review = (TextView) getView().findViewById(R.id.no_customer_review);
-        customer_reviews.setEmptyView(no_customer_review);
+        customer_review_listview.setEmptyView(no_customer_review);
+        customer_review_listview.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                if (customer_review_listview.isGroupExpanded(groupPosition)) {
+                    customer_review_listview.collapseGroupWithAnimation(groupPosition);
+                } else {
+                    customer_review_listview.expandGroupWithAnimation(groupPosition);
+                }
+                return true;
+            }
+        });
+
+        customer_review_listview.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                if (customer_review_listview.isGroupExpanded(groupPosition)) {
+                    customer_review_listview.collapseGroupWithAnimation(groupPosition);
+                } else {
+                    customer_review_listview.expandGroupWithAnimation(groupPosition);
+                }
+                return true;
+            }
+        });
         performRequest();
     }
 }
--- app/src/main/java/veme/cario/com/CARmera/util/ReviewListAdapter.java
+++ app/src/main/java/veme/cario/com/CARmera/util/ReviewListAdapter.java
@@ -4,64 +4,131 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
 import veme.cario.com.CARmera.R;
-import veme.cario.com.CARmera.model.Json.CustomerReview;
 
-/**
- * Created by bski on 11/25/14.
- */
-public class ReviewListAdapter extends ArrayAdapter<CustomerReview> {
-    private LayoutInflater inflater;
-    private static class ViewHolder {
-        TextView reviewer_name_textview;
-        TextView review_title_textview;
-        TextView review_text_textview;
-        TextView review_fav_features_textview;
-        TextView review_sug_improvements_textview;
-        TextView average_rating_textview;
+
+public class ReviewListAdapter extends AnimatedExpandableListView.AnimatedExpandableListAdapter {
+
+    public static class ReviewItem {
+        public String name;
+        public String title;
+        public String rating;
+        public List<ReviewChildItem> items = new ArrayList<ReviewChildItem>();
     }
 
+    public static class ReviewHolder {
+        public TextView name_tv;
+        public TextView title_tv;
+        public TextView rating_tv;
+    }
+
+    public static class ReviewChildItem {
+       public String text;
+       public String favorite_features;
+       public String suggested_improvements;
+    }
+
+    public static class ReviewChildHolder {
+        public TextView text_tv;
+        public TextView favorite_feature_tv;
+        public TextView suggested_imp_tv;
+    }
+
+    private LayoutInflater inflater;
+    private List<ReviewItem> items ;
+
     public ReviewListAdapter(Context context) {
-        super(context, 0);
-        inflater = (LayoutInflater) getContext().getSystemService(
-                Context.LAYOUT_INFLATER_SERVICE);
+        inflater = LayoutInflater.from(context);
+        items = new ArrayList<ReviewItem>();
+    }
+
+    public void setData(List<ReviewItem> items) {
+        this.items = items;
     }
 
     @Override
-    public View getView(int position, View view, ViewGroup parent) {
-        ViewHolder holder;
+    public ReviewChildItem getChild(int groupPosition, int childPosition) {
+        return items.get(groupPosition).items.get(childPosition);
+    }
 
-        if (view == null) {
-            view = inflater.inflate(R.layout.list_item_review, parent, false);
-            holder = new ViewHolder();
-            holder.reviewer_name_textview = (TextView) view.findViewById(R.id.reviewer_name_textview);
-            holder.review_title_textview = (TextView) view.findViewById(R.id.review_title_textview);
-            holder.review_text_textview = (TextView) view.findViewById(R.id.review_text_textview);
-            holder.review_fav_features_textview = (TextView) view.findViewById(R.id.review_favorite_features);
-            holder.review_sug_improvements_textview = (TextView) view.findViewById(R.id.review_suggested_improvements);
-            holder.average_rating_textview = (TextView) view.findViewById(R.id.review_average_rating);
-            view.setTag(holder);
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public View getRealChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        ReviewChildHolder holder;
+        ReviewChildItem item = getChild(groupPosition, childPosition);
+        if (convertView == null) {
+            holder = new ReviewChildHolder();
+            convertView = inflater.inflate(R.layout.list_item_review, parent, false);
+            holder.text_tv = (TextView) convertView.findViewById(R.id.review_text_textview);
+            holder.favorite_feature_tv = (TextView) convertView.findViewById(R.id.review_favorite_features);
+            holder.suggested_imp_tv = (TextView) convertView.findViewById(R.id.review_suggested_improvements);
+            convertView.setTag(holder);
         } else {
-            holder = (ViewHolder) view.getTag();
+            holder = (ReviewChildHolder) convertView.getTag();
         }
 
-        final CustomerReview review = getItem(position);
-        TextView reviewer_name_tv = holder.reviewer_name_textview;
-        TextView review_title_tv = holder.review_title_textview;
-        TextView review_text_tv = holder.review_text_textview;
-        TextView review_ff_tv = holder.review_fav_features_textview;
-        TextView review_si_tv = holder.review_sug_improvements_textview;
-        TextView average_rating_textview = holder.average_rating_textview;
-
-        reviewer_name_tv.setText(review.getAuthor().getAuthorName());
-        review_title_tv.setText(review.getTitle());
-        review_text_tv.setText(review.getText());
-        review_ff_tv.setText(review.getFavoriteFeatures());
-        review_si_tv.setText(review.getSuggestedImprovements());
-        average_rating_textview.setText(review.getAverageRating());
-
-        return view;
+        holder.text_tv.setText(item.text);
+        holder.suggested_imp_tv.setText(item.suggested_improvements);
+        holder.favorite_feature_tv.setText(item.favorite_features);
+        return convertView;
     }
+
+    @Override
+    public int getRealChildrenCount(int groupPosition) {
+        return items.get(groupPosition).items.size();
+    }
+
+    @Override
+    public ReviewItem getGroup(int groupPosition) {
+        return items.get(groupPosition);
+    }
+
+    @Override
+    public int getGroupCount() {
+        return items.size();
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        ReviewHolder holder;
+        ReviewItem item = getGroup(groupPosition);
+        if (convertView == null) {
+            holder = new ReviewHolder();
+            convertView = inflater.inflate(R.layout.review_item, parent, false);
+            holder.name_tv = (TextView) convertView.findViewById(R.id.reviewer_name_textview);
+            holder.title_tv = (TextView) convertView.findViewById(R.id.review_title_textview);
+            holder.rating_tv = (TextView) convertView.findViewById(R.id.review_average_rating);
+            convertView.setTag(holder);
+        } else {
+            holder = (ReviewHolder) convertView.getTag();
+        }
+
+        holder.name_tv.setText(item.name);
+        holder.title_tv.setText(item.title);
+        holder.rating_tv.setText(item.rating + " out of 5");
+
+        return convertView;
+    }
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public boolean isChildSelectable(int arg0, int arg1) {
+        return true;
+    }
+
 }
